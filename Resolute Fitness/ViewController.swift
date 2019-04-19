e31ac81780a36a54d804681e0a6520d837d437c9
Hunter North <hunternorth@D-10-18-163-97.dhcp4.washington.edu>
2019-04-19 16:58:33 -0700

    First Commit

---
+++ Resolute Fitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resolute Fitness
+//
+//  Created by Hunter North on 4/19/19.
+//  Copyright © 2019 Hunter North. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Resolute FitnessTests/Resolute_FitnessTests.swift
@@ -0,0 +1,34 @@
+//
+//  Resolute_FitnessTests.swift
+//  Resolute FitnessTests
+//
+//  Created by Hunter North on 4/19/19.
+//  Copyright © 2019 Hunter North. All rights reserved.
+//
+
+import XCTest
+@testable import Resolute_Fitness
+
+class Resolute_FitnessTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
